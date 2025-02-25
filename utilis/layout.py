afbfec92b520f64518e8f7684e55adfff00f4153
DaoChaShao <27882272@qq.com>
2025-02-26 11:22:22 +1300

    feat: implement pages layout function for sidebar navigation

--- utilis/layout.py
+++ utilis/layout.py
@@ -5,3 +5,24 @@
 # @Version  :   Version 0.1.0
 # @File     :   layout.py
 # @Desc     :
+
+from streamlit import Page, navigation
+
+
+def pages_layout():
+    elements: dict = {
+        "page": ["subpages/home.py", "subpages/chatbot.py"],
+        "title": ["Home", "Chatbot"],
+        "icon": [":material/home:", ":material/smart_toy:"],
+    }
+
+    structure: dict = {
+        "Introduction": [
+            Page(page=elements["page"][0], title=elements["title"][0], icon=elements["icon"][0]),
+        ],
+        "Models": [
+            Page(page=elements["page"][1], title=elements["title"][1], icon=elements["icon"][1]),
+        ],
+    }
+    pg = navigation(structure, position="sidebar", expanded=True)
+    pg.run()
