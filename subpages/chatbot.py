07a13df475d49856c45c052864d12505f3633843
DaoChaShao <27882272@qq.com>
2025-02-26 14:36:42 +1300

    feat: enhance chatbot functionality with API key validation and chat history management

--- subpages/chatbot.py
+++ subpages/chatbot.py
@@ -6,6 +6,63 @@
 # @File     :   chatbot.py
 # @Desc     :
 
-from utilis.tools import parameters
+from streamlit import (chat_input, empty, chat_message, write, sidebar,
+                       session_state, button, caption, rerun)
+
+from utilis.models import Opener
+from utilis.tools import parameters, Timer, api_key_checker
+
+# Initialize the chat history
+if "messages" not in session_state:
+    session_state.messages = []
+
+empty_message: empty = empty()
 
 model_name, api_key, content, temperature, top_p = parameters()
+prompt: str = chat_input("Say something", max_chars=100)
+
+if model_name != "Select a Model":
+
+    # Display the chat history firstly
+    for msg in session_state.messages:
+        with chat_message(msg["role"]):
+            write(msg["content"])
+
+    if api_key:
+        if api_key_checker(api_key):
+            empty_message.success("The API key has been set.")
+            if prompt:
+                # Add the user input to the chat history
+                session_state.messages.append({"role": "user", "content": prompt})
+                # Display the user input
+                with chat_message("user"):
+                    write(prompt)
+
+                # Create a full prompt with the conversation history
+                prompt_all = "\n".join([msg["content"] for msg in session_state.messages])
+
+                with Timer(2, description="Local Model Call") as timer:
+                    with chat_message("assistant"):
+                        response = Opener(api_key, temperature, top_p).client(prompt, content, model_name)  # 生成 AI 回复
+                        write(response)
+
+                        # Add the AI response to the chat history
+                        session_state.messages.append({"role": "assistant", "content": response})
+
+                empty_message.success(f"**{str(timer)}**")
+            else:
+                empty_message.error("Please enter a prompt to generate a response.")
+    else:
+        empty_message.error("Please enter the API key for the model.")
+else:
+    empty_message.info("Please select a model from the sidebar.")
+
+with sidebar:
+    if len(session_state.messages) > 0:
+        if button("Clear History", type="primary", help="Click to clear the chat history."):
+            session_state.messages = []
+            empty_message.success("The history has been cleared.")
+            rerun()
+        caption(f"**{int(len(session_state.messages) / 2)}** round messages in the chat history.")
+    else:
+        caption("The chat history is empty.")
