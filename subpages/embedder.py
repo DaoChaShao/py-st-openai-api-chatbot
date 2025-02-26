4c3c53aef8076ca9897e938c3143ac4a76871d20
DaoChaShao <27882272@qq.com>
2025-02-26 17:12:35 +1300

    feat: add embedder functionality with API key validation and text embedding

---
+++ subpages/embedder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/26 14:37
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   embedder.py
+# @Desc     :
+
+from pandas import DataFrame
+from streamlit import (empty, sidebar, button, balloons, spinner,
+                       data_editor, plotly_chart)
+
+from utilis.chart_plot import PCALearnerDimensionsReducer, scatter_2d
+from utilis.models import Embedder
+from utilis.tools import parameters_embedder, Timer, api_key_checker
+
+empty_message: empty = empty()
+
+model_name, api_key, thing, stuff = parameters_embedder()
+
+if api_key and api_key_checker(api_key):
+    empty_message.success("The API key has been set.")
+
+    if thing and stuff:
+        prompt: list = [thing, stuff]
+        with sidebar:
+            embed = button("Embedder", type="primary", help="Embed the text.")
+
+        if embed:
+            with spinner("Embedding the text...", show_time=True):
+                with Timer(2, "Embedding Model Call") as timer:
+                    response = Embedder(api_key).client(prompt, model_name)
+
+                    df_embed: DataFrame = DataFrame(response, index=prompt)
+                    data_editor(df_embed, disabled=True, use_container_width=True)
+
+                    # Reduce the dimensions of the features using UMAP
+                    reducer: DataFrame = PCALearnerDimensionsReducer(2).fit(df_embed)
+                    data_editor(reducer, hide_index=True, disabled=True, use_container_width=True)
+
+                    # Display the chart of scatter
+                    chart = scatter_2d(reducer, point_size=10, font_size=12)
+                    plotly_chart(chart)
+
+                    balloons()
+
+                empty_message.success(timer)
+    else:
+        empty_message.error("Please enter the text to embed.")
+else:
+    empty_message.error("Please enter the API key for the model.")
